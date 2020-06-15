53bb50e19591d9d803679675fdb522f3f627760a
EricFidalgo <49496282+EricFidalgo@users.noreply.github.com>
2020-06-15 13:00:02 -0400

    Add files via upload

--- submit.php
+++ submit.php
@@ -1,12 +1,11 @@
-<?php 
- 
-$data_file = fopen("example.txt","w");
+<?php
 
-$name = $_POST["name"]
-$age = $_POST["age"]
+$data_file = fopen("example.txt", "a") ;
+
+$name = $POST["name"] ;
+$age = $_POST["age"];
 $text_to_write = $name . " " . $age;
 
-fwrite ($data_file, $text_to_write) ;
-fclose ($data_file);
-
+fwrite($data_file, $text_to_write);
+fclose($data_file) ;
 ?>
